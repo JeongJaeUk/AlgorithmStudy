9bd17490ba10280b55cc22590c065f5e7c12f786
JaeUk Jeong <skstk465@gmail.com>
2020-03-30 11:33:21 +0900

    init repository with eclipse project

---
+++ BaekJoonAlgorithm/src/baekjoon_200330_13460_구슬탈출2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class baekjoon_200330_13460_구슬탈출2 {
+	public static void main(String[] args) throws Exception {
+		System.setIn(new FileInputStream("res/input13460.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(br.readLine().trim());
+	}
+}
